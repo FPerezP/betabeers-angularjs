31bcea0b63a16e60419f4347b670a1733817232c
Francisco Pérez Paradas <francisco@perezparadas.com>
2013-11-29 15:10:33 +0100

    Adds watch tab

--- js/controllers/watchCtrl.js
+++ js/controllers/watchCtrl.js
@@ -3,15 +3,21 @@
 betabeersControllers.controller('watchCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
   $rootScope.activeTab = 'watch';
   $scope.title = 'Te estoy observando... ¬_¬';
+  $scope.police = 'Todo en orden!';
+  $scope.count = 0;
+  $scope.executed = 'Viendo Starwars :)';
+
+  $scope.$watch('executed', function() {
+    $scope.count++;
+    if ($scope.count < 5) {
+      $scope.police = 'Se ha cambiado ' + $scope.count + ' veces';
+    }
+    else {
+      $scope.police = 'No tienes nada mejor que hacer?';
+    }
+  });
 
   $scope.run = function() {
-    $timeout(function() {
-      $scope.getData('/static/dummy.json').
-        then($scope.getData('/static/dummy1.json')).
-        then($scope.getData('/static/dummy2.json')).
-        then(function() {
-          $scope.executed = "terminé!";
-        });
-    });
+      $scope.executed = ':-O! Run me ha cambiado(' + $scope.count + ')!';
   };
 }]);
