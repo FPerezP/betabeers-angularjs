c2504d6650e5a6d539528a94a8bc822975553f45
Francisco Pérez Paradas <francisco@perezparadas.com>
2013-11-29 15:12:00 +0100

    Improves promises description

--- js/controllers/promisesCtrl.js
+++ js/controllers/promisesCtrl.js
@@ -1,16 +1,17 @@
 'user strict';
 
-betabeersControllers.controller('promisesCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+betabeersControllers.controller('promisesCtrl', ['$scope', '$http', '$rootScope', '$timeout', function($scope, $http, $rootScope, $timeout) {
   $rootScope.activeTab = 'promises';
   $scope.title = 'Lo prometo :-)';
 
   $scope.run = function() {
     $timeout(function() {
       $scope.getData('/static/dummy.json').
-        then($scope.getData('/static/dummy1.json')).
         then($scope.getData('/static/dummy2.json')).
+        then($scope.getData('/static/dummy1.json')).
         then(function() {
           $scope.executed = "terminé!";
+          console.log('Después de la última');
         });
     });
   };
